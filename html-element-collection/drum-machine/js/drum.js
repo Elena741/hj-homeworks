ac1b44826f88f34277223ea770ef99226de74591
Elena741 <solo741@yandex.ru>
2019-02-05 14:43:40 +0300

    Поправила

--- html-element-collection/drum-machine/js/drum.js
+++ html-element-collection/drum-machine/js/drum.js
@@ -2,5 +2,8 @@ const buttons = document.getElementsByTagName('li');
 
 for (const btn of buttons) {
 	const player = btn.getElementsByTagName('audio')[0];
-	btn.onclick = () => player.play();
+	btn.onclick = () => {
+		player.currentTime = 0;
+		player.play();
+	}
 }
