5c890817ae8ad22df67c19f5b24e96b2e55d6e3b
Elena741 <solo741@yandex.ru>
2019-02-22 12:53:55 +0300

    Изменила

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+document.querySelector('.items-list').addEventListener('click', addButton);
+
+function addButton(event) {
+
+	if(event.target.classList.contains('add-to-cart')) {
+		event.preventDefault();		
+		addToCart(event.target.dataset);
+	}
+
+}
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
 'use strict';
-
 function handleTableClick(event) {
-  
+
+  const propName = event.target.dataset.propName;
+
+  if (event.target.dataset.dir === '1') {
+    event.target.dataset.dir = "-1"
+  }
+  else {
+    event.target.dataset.dir = "1";
+  }
+
+  table.dataset.sortBy = propName;
+  sortTable(propName, event.target.dataset.dir);
+
 }
